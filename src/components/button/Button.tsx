e1a96ab653994d5cbfdd970c7210cdb65ab873fb
Krzysztof Podmokły <krzysztof.podmokly@gmail.com>
2025-06-22 17:00:24 +0200

    Adjusting responsive design

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -11,17 +11,15 @@ const Button = ({
   testId,
 }: IButtonProps) => {
   return (
-    <div>
-      <button
-        className={['button', className].filter(Boolean).join(' ')}
-        onClick={onClick}
-        disabled={isDisabled}
-        data-testid={testId}
-      >
-        <span>{text}</span>
-        {icon && <span className="icon">{icon}</span>}
-      </button>
-    </div>
+    <button
+      className={['button', className].filter(Boolean).join(' ')}
+      onClick={onClick}
+      disabled={isDisabled}
+      data-testid={testId}
+    >
+      <span>{text}</span>
+      {icon && <span className="icon">{icon}</span>}
+    </button>
   );
 };
 
